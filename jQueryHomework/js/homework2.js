64887ec8d7297efbbee6fc877150888e4dbc0047
紹瑋 藍 <maimaisatt@gmail.com>
2022-08-16 22:26:33 +0800

    20220816_homework2start

--- jQueryHomework/js/homework2.js
+++ jQueryHomework/js/homework2.js
@@ -1,1 +1,11 @@
-﻿$("#article2 #imgdiv").html('<img src="images/homework2/1.png">');
+﻿let startPlay = setInterval(PlayImage, 2000);
+$("#article2 #imgdiv").html('<img src="images/homework2/1.jpg">');
+let str = "";
+for (let i = 1; i <= 10; i++) {
+    str += `<img src="images/circle.png" class="circleimg">`;
+}
+$("#selectdiv").html(str);
+
+function PlayImage() {
+
+}
